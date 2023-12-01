f45c22d30c90a356f5f053d71bdf2ffe187b9a67
Pravin Bansod <136956809+silentSK987@users.noreply.github.com>
2023-12-01 23:58:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
 function fibonacci(num) {
-function fibonacci(num) {
     // your code here
-      let a=0,b=1,c=0;
-        let fibo=0;
-	           for(let i=1; i < num;i++) {   //0 = 1  //1==1 //2=1 //3=2 //4=3 //5=5 //6==8
-                if(num===1)  fibo =1;
-               else
-               fibo=c;
-               // if (n==i)
-             // System.out.println(i+" "+c);
-               
-               c=a+b;       //0]0+1=1 
-               a=b;         // 0] 0 wil become 1;
-               b=c;         // 0] 1 (thats b) will become = c (thats 1)
-           }
-            return fibo;
+   if (num <= 0) {
+        return "Please enter a positive integer for the Fibonacci sequence.";
+    } else if (num === 1) {
+        return 0;
+    } else if (num === 2) {
+        return 1;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
     }
-    
 }
 
 module.exports = fibonacci;
